a6dcc3861f22d815702e2e494448f0caf816201d
ravi <rafael.vitor@jusbrasil.com.br>
2019-01-12 22:53:43 -0300

    Edit bio

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
+    title: `Aboutstuff`,
+    author: `Ravi`,
+    description: `Description`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `TODO`,
     },
   },
   plugins: [
