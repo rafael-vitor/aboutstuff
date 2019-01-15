70ce410e4aa538c6635f0bb097a44be1810a6db3
ravi <rafael.vitor@jusbrasil.com.br>
2019-01-15 09:20:13 -0300

    Add codepen snippet integration

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -7,6 +7,39 @@ import SEO from '../components/seo'
 import { rhythm, scale } from '../utils/typography'
 
 class BlogPostTemplate extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      'codepen': false
+    };
+  }
+
+  componentDidMount() {
+    // Add Codepen script to <body> if we detect a Codepen embed
+    const codepen = document.getElementsByClassName('codepen');
+    if (codepen.length > 0) {
+      // Check if we've already embedded the script
+      if (!document.getElementById('codepen-script') || !this.state.codepen) {
+        // Create script element with Codepen embed JS lib
+        const s = document.createElement('script')
+        s.async = s.defer = true
+        s.src = `//static.codepen.io/assets/embed/ei.js`
+        s.id = 'codepen-script'
+        const body = document.body
+        if (body) {
+          body.appendChild(s)
+        }
+
+        // Set state to true so the process doesn't run again
+        this.setState({
+          'codepen': true
+        });
+      }
+    }
+  }
+
+
   render() {
     const post = this.props.data.markdownRemark
     const siteTitle = this.props.data.site.siteMetadata.title
