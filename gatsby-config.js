ccbbfcadc983ee45e84c2660db9893e478340c14
ravi <rafael.vitor@jusbrasil.com.br>
2019-01-15 09:19:46 -0300

    Add gatsby twitter plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,6 +47,7 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-twitter`,
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
